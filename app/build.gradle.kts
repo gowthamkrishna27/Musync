ed00e6f2d55776454f8b481b14619d8b9d161607
gowthamkrishna27 <gowthamkrishna18v@gmail.com>
2026-08-16 12:59:48 +0530

    chore(release): bump version to v1.1.3.1 (patch release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.musync.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1130
-        versionName = "1.1.3"
+        versionCode = 1131
+        versionName = "1.1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
