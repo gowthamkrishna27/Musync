b624559bfed621a509e4ddc9267e5c56b032ef1e
gowthamkrishna27 <gowthamkrishna18v@gmail.com>
2026-08-21 11:37:03 +0530

    Release v1.1.8.2 - True 1:1 Uncompressed Original Logo & Adaptive Icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.musync.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1181
-        versionName = "1.1.8.1"
+        versionCode = 1182
+        versionName = "1.1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
